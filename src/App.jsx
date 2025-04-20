b32dd3cb95c4e40ab3a8fb5472c429fed3c25d7d
fatimaOuali <fatimaouali965@gmail.com>
2025-04-20 13:49:14 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Particle from './components/Particle'
+
+
+const App = () => {
+  return (
+    <div className='min-h-screen'>
+      <Particle />
+    </div>
+  )
+}
+
+export default App
